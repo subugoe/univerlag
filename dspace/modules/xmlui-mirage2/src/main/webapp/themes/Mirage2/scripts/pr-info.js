9ba3782172059c1188edfcbcc672c2e3655ae7dc
dspace dev <>
2018-08-06 14:09:08 +0000

    Add js to retrieve PR data from DOAB

---
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/pr-info.js
@@ -0,0 +1,32 @@
+$(function(){
+  var isbn = $("#isbn").text();
+  var detailsElement = $("#details");
+
+  var doabUrl = 'https://www.doabooks.org/api/peerReviews';
+  var doabQuery = doabUrl + '?isbn=' + isbn;
+
+  $.ajax({
+    type: "GET",
+    url: doabQuery,
+    contentType: false,
+    error: function(){
+      // nothing to do
+    },
+    success: function(response){
+      addPrDescription(response);
+    },
+    timeout: 3000 // sets timeout to 3 seconds
+  });
+
+  function addPrDescription(data) {
+    var prIcon = data.PeerReviews[0].prIconUrl;
+    var doabBookUrl = data.book.bUrl;
+    var prIconTag = "<p><strong>Peer reviewed</strong>: " +
+        "<a href=" + doabBookUrl + " title='Peer review process certified by DOAB' target='_blank'>" +
+          "<img src=" + prIcon + " alt='certified by DOAB'>" +
+        "</a>" +
+      "</p>"
+    detailsElement.append(prIconTag);
+  }
+
+});
